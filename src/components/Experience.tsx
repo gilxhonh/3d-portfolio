9539fceb02f210f66499628a4f54a5fa18f9b3d5
Gilxhon Hima <gilxhonhima@gmail.com>
2023-12-14 00:46:23 +0100

    fix animations + add wireframe

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,18 +1,30 @@
-import {OrbitControls} from "@react-three/drei";
-import {Avatar} from "./Avatar";
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
+import { Avatar } from "./Avatar.tsx";
 
 const Experience = () => {
-    return (
-        <>
-            <OrbitControls/>
-            <group position-y={-1}>
-                <Avatar/>
-            </group>
-            <ambientLight intensity={1}/>
-            <directionalLight intensity={2} position={[10, 20, 10]} shadow-radius={5} castShadow/>
-            <planeGeometry/>
-        </>
-    );
+  const { animation } = useControls({
+    animation: {
+      value: "Typing",
+      options: ["Typing", "Bored", "FallingIdle"],
+    },
+  });
+  return (
+    <>
+      <OrbitControls />
+      <group position-y={-1}>
+        <Avatar animation={animation} />
+      </group>
+      <ambientLight intensity={1} />
+      <directionalLight
+        intensity={2}
+        position={[10, 20, 10]}
+        shadow-radius={5}
+        castShadow
+      />
+      <planeGeometry />
+    </>
+  );
 };
 
 export default Experience;
