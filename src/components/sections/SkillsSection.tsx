a78b4029ee49dc67538f2170b898f99f2a6a8b63
Gilxhon Hima <gilxhonhima@gmail.com>
2023-12-21 02:29:33 +0100

    Update SkillsSection.tsx

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Section from "../core/Section.tsx";
-import ProgressBar from "../core/ProgressBar.tsx";
 
 type Skill = {
   title: string;
@@ -15,61 +14,115 @@ type Language = {
 
 // Skills and Languages Data
 const skills: Skill[] = [
-  { title: "ReactJs / React Native", level: 90 },
-  { title: "Threejs / React Three Fiber", level: 90 },
-  { title: "JavaScript", level: 80 },
-  { title: "Typescript", level: 70 },
+  { title: "Git / GitHub", level: 95 },
+  { title: "TypeScript", level: 90 },
+  { title: "JavaScript", level: 85 },
+  { title: "React Js / React Native", level: 100 },
+  { title: "Redux", level: 70 },
+  { title: "Three Js / React Three Fiber", level: 45 },
+  { title: "PixiJs", level: 45 },
 ];
 
 const languages: Language[] = [
   { title: "🇦🇱 Albanian", level: 100 },
-  { title: "🇺🇸 English", level: 90 },
-  { title: "🇮🇹 Italian", level: 25 },
+  { title: "🇺🇸 English", level: 95 },
 ];
 
-// Skill Item Component
-type SkillItemProps = {
-  skill: Skill;
-  index: number;
+const SkillsSection: React.FC = () => {
+  return (
+    <Section>
+      <motion.div whileInView={"visible"}>
+        <h2 className="text-5xl font-bold">Skills</h2>
+        <div className=" mt-8 space-y-4">
+          {skills.map((skill, index) => (
+            <div className="w-64" key={index}>
+              <motion.h3
+                className="text-xl font-bold text-gray-800"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 1 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {skill.title}
+              </motion.h3>
+              <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                <motion.div
+                  className="h-full bg-indigo-500 rounded-full "
+                  style={{ width: `${skill.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-5xl font-bold mt-10">Languages</h2>
+          <div className=" mt-8 space-y-4">
+            {languages.map((lng, index) => (
+              <div className="w-64" key={index}>
+                <motion.h3
+                  className="text-xl font-bold text-gray-800"
+                  initial={{
+                    opacity: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 2 + index * 0.2,
+                      },
+                    },
+                  }}
+                >
+                  {lng.title}
+                </motion.h3>
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                  <motion.div
+                    className="h-full bg-indigo-500 rounded-full "
+                    style={{ width: `${lng.level}%` }}
+                    initial={{
+                      scaleX: 0,
+                      originX: 0,
+                    }}
+                    variants={{
+                      visible: {
+                        scaleX: 1,
+                        transition: {
+                          duration: 1,
+                          delay: 2 + index * 0.2,
+                        },
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </motion.div>
+    </Section>
+  );
 };
-
-const SkillItem: React.FC<SkillItemProps> = ({ skill, index }) => (
-  <>
-    <div className="w-64">
-      <motion.h3
-        className="text-xl font-bold text-gray-800"
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: { duration: 1, delay: 1 + index * 0.2 },
-        }}
-      >
-        {skill.title}
-      </motion.h3>
-    </div>
-    <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
-      <ProgressBar level={skill.level} />
-    </div>
-  </>
-);
-
-// Skills Section Component
-const SkillsSection: React.FC = () => (
-  <Section>
-    <h2 className="text-5xl font-bold">Skills</h2>
-    <div className="mt-8 space-y-4">
-      {skills.map((skill, index) => (
-        <SkillItem skill={skill} index={index} key={index} />
-      ))}
-    </div>
-
-    <h2 className="text-5xl font-bold mt-10">Languages</h2>
-    <div className="mt-8 space-y-4">
-      {languages.map((language, index) => (
-        <SkillItem skill={language} index={index} key={index} />
-      ))}
-    </div>
-  </Section>
-);
 
 export default SkillsSection;
