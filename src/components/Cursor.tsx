9615dd063f93a6fd6d68c784654fa4335f4e980a
Gilxhon Hima <gilxhonhima@gmail.com>
2023-12-22 12:05:22 +0100

    Update Cursor.tsx

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -45,7 +45,9 @@ const Cursor: React.FC = () => {
       const target = e.target as HTMLElement;
       if (
         target.tagName.toLowerCase() === "button" ||
-        target.parentElement?.tagName.toLowerCase() === "button" ||
+        (target.parentElement?.tagName.toLowerCase() === "a" &&
+          target.parentElement?.tagName.toLowerCase() === "button") ||
+        target.tagName.toLowerCase() === "span" ||
         target.tagName.toLowerCase() === "input" ||
         target.tagName.toLowerCase() === "textarea"
       ) {
